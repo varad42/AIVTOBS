e4b50e20f1d90bf07842134dfcfb060664fa37fb
studvarad <borole.varad.it@kgce.edu.in>
2026-03-30 17:04:24 +0530

    gunicorn

--- worker.py
+++ worker.py
@@ -1,10 +1,9 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from modules.queue_worker import preload_whisper_model, recover_interrupted_jobs, worker_loop
+from modules.queue_worker import preload_whisper_model, worker_loop
 
 
 if __name__ == "__main__":
-    recover_interrupted_jobs()
     preload_whisper_model()
     worker_loop()
